06be3cd9bd708770229a49f93da1c85877773a99
notalentgeek <mikael.pratama@yahoo.com>
2016-02-03 21:28:06 +0100

    Adding comments to CollectionEnum.hx.

--- Source/CollectionEnum.hx
+++ Source/CollectionEnum.hx
@@ -1,22 +1,62 @@
+/*==================================================
+An enum that controls museum type.
+EXH is for any museum object that is exhibition.
+FLR is for any museum object that is floor.
+ROM is for any museum object that is room.*/
 enum EnumMuseumType{
     EXH;
     FLR;
     ROM;
 }
+/*==================================================*/
+
+
+
+/*==================================================
+This is an enum for controlling object museum update mode before deletion.
+REQ_CH_PARENT is for any museum object that is still has children objects 
+    attached to it.
+So the user need to remove any children of the museum object with REQ_CH_PARENT
+    to a new parent.
+After there is no children object anymore than this museum object changed its
+    mode to MRK_DEL and then deletion of the corresponding museum object begins.*/
 enum EnumMuseumMode{
     REQ_CH_PARENT;
     MRK_DEL;
 }
+/*==================================================*/
+
+
+
+/*==================================================
+An enum to control how a visitor object updated for each ticks.
+HARDWARE_MANUAL is for visitor object that is controlled by hardware.
+SOFTWARE_AUTO is for visitor object so that it is controlled by this program
+    artificial intelligence.
+SOFTWARE_MANUAL means that visitor object to be controlled manually by using
+    this program.*/
 enum EnumVisitorMode{
     HARDWARE_MANUAL;
     SOFTWARE_AUTO;
     SOFTWARE_MANUAL;
 }
+/*==================================================*/
+
+
+
+/*==================================================
+This enumeration is for giving emotion for every tag entered to this program*/
 enum EnumTagFeelType{
     NEGATIVE;
     NEUTRAL;
     POSITIVE;
 }
+/*==================================================*/
+
+
+
+/*==================================================
+Enumeration for tag types.*/
 enum EnumTagType{
     ADJ;
     ADV;
@@ -35,6 +75,12 @@ enum EnumTagType{
     VERB_INTRANSITIVE;
     VERB_TRANSITIVE;
 }
+/*==================================================*/
+
+
+
+/*==================================================
+Enumeration for tag sub types.*/
 enum EnumTagTypeSub{
     ADJ;
     ADV;
@@ -48,3 +94,4 @@ enum EnumTagTypeSub{
     VERB_ING;
     VERB_S;
 }
+/*==================================================*/
