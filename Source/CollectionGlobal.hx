11e0703140cc2c1a8f8e7baaed49446a6cb9a101
notalentgeek <mikael.pratama@yahoo.com>
2016-03-21 18:31:14 +0100

    Done with pre - made museum class object and the tag object. Next thing is to re - make the pre - made tag object both general class and specific tag object class.

--- Source/CollectionGlobal.hx
+++ Source/CollectionGlobal.hx
@@ -1,17 +1,3 @@
-import CollectionEnum;
-import CollectionFunction;
-import CollectionStruct;
-import flash.display.*;
-import flash.events.*;
-import haxe.ui.toolkit.containers.*;
-import haxe.ui.toolkit.core.*;
-import haxe.ui.toolkit.core.interfaces.*;
-import haxe.ui.toolkit.events.*;
-
-
-
-
-
 class CollectionGlobal{
 
 
@@ -19,43 +5,41 @@ class CollectionGlobal{
 
 
     /*==================================================
-    Private variables.
-    PENDING: Change all of these into static "global" variables.
-    Archive exhibition is a reserve exhibition to put every player that has removed.
-    PENDING (OBSELETE): Make archive object for floor and room to contain all deleted
-        exhibitions, rooms, and floors.
-    PENDING: Change the name this variable from archiveExhibitionObject into
-        archiveVisitorObject.
-    PENDING: Create object array for each deleted exhibitions, floors, and rooms. Let say
-        name it archiveExhibitionObjectArrray, archiveFloorObjectArray, and
-        archiveRoomObjectArray.
-    PENDING: Make so that a visitor that has finished to visit the museum to change current
-        exhibition archive visitor object.*/
-    private var archiveExhibitionObject             :ObjectMuseum                           = null;
-    /*Amount of visitor necessary so that a museum object can be defined as full capacity.*/
-    private var exhibitionFullThresholdInt          :Int                                    = 0;
-    /*An array to hold all the exhibition objects.*/
-    private var exhibitionObjectArray               :Array              <ObjectMuseum>      = new Array<ObjectMuseum>();
-    /*An array to hold all the floor objects.*/
-    private var floorObjectArray                    :Array              <ObjectMuseum>      = new Array<ObjectMuseum>();
-    /*Visitor index, this variable will increment one for everytime a visitor is added to the scene.*/
-    private var indexGlobalVisitorInt               :Int                                    = 0;
-    /*Variable to define offset on museum buttons.
-    PENDING: Change this into more UI centric variable, probably changed into uiMuseumOffsetInt.*/
-    private var museumUIOffsetInt                   :Int                                    = 5;
-    /*An array to hold all the room objects.*/
-    private var roomObjectArray                     :Array              <ObjectMuseum>      = new Array<ObjectMuseum>();
-    /*This array contains common words that often to be put as tag in museum object.*/
-    private var tagGeneralObjectArray               :Array              <ObjectTag>         = new Array<ObjectTag>();
-    /*More specific tag object array.*/
-    private var tagObjectArray                      :Array              <ObjectTag>         = new Array<ObjectTag>();
-    /*Main user interface when the program first starts.*/
-    private var uiMainObject                        :IDisplayObject                         = null;
+    PENDING: At this moment I have no idea yet on how/where to put deleted floor museum object.
+    PENDING: Change every deleted exhibition object into archive room object array.
+    PENDING: Change every deleted room object into archive floor object array.
+    PENDING: Change every finished and deleted visitor into archive exhibition object array.
+    PENDING: Change this class name from CollectionGlobal.hx into Global_Object.hx*/
     /*Absolute layout object for all object museum buttons and all visitor object buttons.*/
-    private var uiMuseumAbsoluteObject              :Absolute                               = null;
-    /*A array to hold all visitor.
-    PENDING: Probably add a support to database in the future.*/
-    private var visitorObjectArray                  :Array              <ObjectVisitor>     = new Array<ObjectVisitor>();
+    private var _Absolute(null, null):Absolute = null;
+    /*Main user interface when the program first starts.*/
+    private var _IDisplayObject(null, null):IDisplayObject = null;
+    /*Specific tag object array.*/
+    private var _Tag_Object_Array(null, null):Array<Tag_Object> = new Array<Tag_Object>();
+    /*A array to hold all visitor.*/
+    private var _Visitor_Object_Array(null, null):Array<Visitor_Object> = new Array<Visitor_Object>();4
+    /*A pseudo museum object archive exhibition object for storing deleted or finished visitor.*/
+    private var archiveExhibition_Museum_Object(null, null):Museum_Object = null;
+    /*A pseudo museum object archive floor object for storing deleted room museum object.*/
+    private var archiveFloor_Museum_Object(null, null):Museum_Object = null;
+    /*A pseudo museum object archive room object for storing deleted exhibition museum object.*/
+    private var archiveRoom_Museum_Object(null, null):Museum_Object = null;
+    /*An array to hold all the exhibition objects.*/
+    private var exhibition_Museum_Object_Array(null, null):Array<Museum_Object> = new Array<Museum_Object>();
+    /*An array to hold all the floor objects.*/
+    private var floor_Museum_Object_Array(null, null):Array<Museum_Object> = new Array<Museum_Object>();
+    /*Amount of visitor necessary so that a museum object can be defined as full capacity.*/
+    private var fullThreshold_Int(null, null):Int = 0;
+    /*This array contains common words that often to be put as tag in museum object.*/
+    private var general_Tag_Object_Array(null, null):Array<Tag_Object> = new Array<Tag_Object>();
+    /*Visitor index, this variable will increment one for everytime a visitor is added to the scene.*/
+    private var indexGlobalVisitor_Int(null, null):Int = 0;
+    /*Another pseudo museum object that holds visitor before they visit their first exhibition.*/
+    private var lobby_Museum_Object(null, null):Museum_Object = null;
+    /*Variable to define offset on museum buttons.*/
+    private var museumUIOffset_Int(null, null):Int = 5;
+    /*An array to hold all the room objects.*/
+    private var room_Museum_Object_Array(null, null):Array<Museum_Object> = new Array<Museum_Object>();
     /*==================================================*/
 
 
@@ -64,10 +48,14 @@ class CollectionGlobal{
 
     /*==================================================
     Constructor.*/
-    public function new(){
-        /*In this constructor I only put on to initialize the archive exhibition object.
-        PENDING: Please add archive for floor and room object museum.*/
-        archiveExhibitionObject = new ObjectMuseum(this, ["No explanation."], "EXH_ARC", "Exhibition Archive", "XXX_XXX", new Array<ObjectTag>(), EXH);
+    public function new():Void{
+
+        /*In this constructor I only put on to initialize the pseudo museum objects.
+        PENDING - DONE: Please add archive for floor and room object museum.*/
+        archiveExhibition_Museum_Object     = new Museum_Object(this, ["No explanation."], "EXH_ARC", "Exhibition Archive", "ROM_ARC", new Array<Tag_Object>(), EXH);
+        archiveFloor_Museum_Object          = new Museum_Object(this, ["No explanation."], "FLR_ARC", "Floor Archive", "XXX_XXX", new Array<Tag_Object>(), FLR);
+        archiveRoom_Museum_Object           = new Museum_Object(this, ["No explanation."], "ROM_ARC", "Room Archive", "FLR_ARC", new Array<Tag_Object>(), ROM);
+
     }
     /*==================================================*/
 
@@ -77,19 +65,29 @@ class CollectionGlobal{
 
     /*==================================================
     A function to determine what number makes a museum object is full.*/
-    public function DetermineExhibitionFullThresholdVoid(){
-        /*This can be done by comparing the amount of exhibition that visitor can visit and total
-            amount of visitor.
-        PENDING: Change so that this does not make count to the visitors that have finished visiting
-            the museum.*/
-        if(exhibitionObjectArray.length >= visitorObjectArray.length){
-            exhibitionFullThresholdInt =
-                Math.ceil(exhibitionObjectArray.length/visitorObjectArray.length);
+    public function DetermineFullThreshold_Int():Int{
+
+        /*This can be done by comparing the amount of exhibition that visitor can visit and total amount of visitor.
+        PENDING: Change so that this does not make count to the visitors that have finished visiting the museum.*/
+        if(exhibition_Museum_Object_Array.length >= _Visitor_Object_Array.length){
+
+            fullThreshold_Int =
+                Math.ceil(exhibition_Museum_Object_Array.length/_Visitor_Object_Array.length);
+
         }
-        else if(exhibitionObjectArray.length < visitorObjectArray.length){
-            exhibitionFullThresholdInt =
-                Math.ceil(visitorObjectArray.length/exhibitionObjectArray.length);
+        else if(exhibition_Museum_Object_Array.length < _Visitor_Object_Array.length){
+
+            fullThreshold_Int =
+                Math.ceil(_Visitor_Object_Array.length/exhibition_Museum_Object_Array.length);
+
         }
+
+
+
+
+
+        return fullThreshold_Int;
+
     }
     /*==================================================*/
 
@@ -99,42 +97,21 @@ class CollectionGlobal{
 
     /*==================================================
     A function to increament the the global visitor index.*/
-    public function PutIndexGlobalVisitorInt(){
+    public function IncrementIndexGlobalVisitor_Int():Int{
+
         /*I got a problem here of which I need to return a value then increment the value.
         The solution is to copy the integer variable, incremenet the global index value, 
             and then return the copied value.*/
-        var tempIndexGlobalVisitorInt:Int = indexGlobalVisitorInt;
-        indexGlobalVisitorInt ++;
+        var tempIndexGlobalVisitorInt:Int = indexGlobalVisitor_Int;
+        indexGlobalVisitor_Int ++;
+
+
+
+
+
         return tempIndexGlobalVisitorInt;
+
     }
-    /*==================================================*/
-
-
-
-
-
-    /*==================================================
-    Basic getter and setter functions.*/
-    public function GetUIMuseumAbsoluteObject           (){ return uiMuseumAbsoluteObject;      }
-    public function GetArchiveExhibitionObject          (){ return archiveExhibitionObject;     }
-    public function GetExhibitionFullThresholdInt       (){ return exhibitionFullThresholdInt;  }
-    public function GetExhibitionObjectArray            (){ return exhibitionObjectArray;       }
-    public function GetFloorObjectArray                 (){ return floorObjectArray;            }
-    public function GetIndexGlobalVisitorInt            (){ return indexGlobalVisitorInt;       }
-    public function GetMuseumUIOffsetInt                (){ return museumUIOffsetInt;           }
-    public function GetRoomObjectArray                  (){ return roomObjectArray;             }
-    public function GetTagGeneralObjectArray            (){ return tagGeneralObjectArray;       }
-    public function GetTagObjectArray                   (){ return tagObjectArray;              }
-    public function GetUIMainObject                     (){ return uiMainObject;                }
-    public function GetVisitorObjectArray               (){ return visitorObjectArray;          }
-    public function SetUIMuseumAbsoluteObjectVoid       (_absoluteObject                :Absolute                           ){ uiMuseumAbsoluteObject       = _absoluteObject;                  }
-    public function SetUIMainObjectVoid                 (_iDisplayObject                :IDisplayObject                     ){ uiMainObject                 = _iDisplayObject;                  }
-    public function SetExhibitionFullThresholdInt       (_exhibitionFullThresholdInt    :Int                                ){ exhibitionFullThresholdInt   = _exhibitionFullThresholdInt;      }
-    public function SetExhibitionObjectArrayVoid        (_exhibitionObjectArray         :Array              <ObjectMuseum>  ){ exhibitionObjectArray        = _exhibitionObjectArray;           }
-    public function SetFloorObjectArrayVoid             (_floorObjectArray              :Array              <ObjectMuseum>  ){ floorObjectArray             = _floorObjectArray;                }
-    public function SetRoomObjectArrayVoid              (_roomObjectArray               :Array              <ObjectMuseum>  ){ roomObjectArray              = _roomObjectArray;                 }
-    public function SetTagObjectArrayVoid               (_tagObjectArray                :Array              <ObjectTag>     ){ tagObjectArray               = _tagObjectArray;                  }
-    public function SetVisitorObjectArrayVoid           (_visitorObjectArray            :Array              <ObjectVisitor> ){ visitorObjectArray           = _visitorObjectArray;              }
     /*==================================================*/
 
 
--- Source/Tag_Object.hx
+++ Source/Tag_Object.hx
@@ -6,13 +6,13 @@ class Tag_Object{
 
     /*PENDING - DONE: Create function to remove this tag object from main array. Name it RemoveThisFromMain_Tag_Object
     PENDING - DONE: Create function to add this object into main array.
-    PENDING: Create function to generate company words.
-    PENDING: Create the constructor.*/
+    PENDING - DONE: Create function to generate company words.
+    PENDING - DONE: Create the constructor.*/
 
 
 
 
-
+    /*==================================================*/
     private var _TagAgnostic_Object         (null, null)    :TagAgnostic_Object         = null;
 
 
@@ -28,34 +28,74 @@ class Tag_Object{
 
 
     private var _CompanyWord_Struct_Array   (null, null)    :Array<CompanyWord_Struct>  = new Array<CompanyWord_Struct>();  /*PENDING: Create setter function to tied this variable wit its agnostic couterpart.*/
+    /*==================================================*/
 
 
 
 
 
-    public function new():Void{ _TagAgnostic_Object = new TagAgnostic_Object(); }
+    /*==================================================*/
+    public function new(
+        __CollectionGlobal_Object   :CollectionGlobal,
+        __TagFeel_Enum              :EnumTagFeelType,
+        __TagType_String            :EnumTagType,
+        _generalTagBool             :Bool,
+        _nameOriginal_String        :String
+    ):Void{
+
+        /*Assign all paremeters.*/
+        _CollectionGlobal_Object    = __CollectionGlobal_Object;
+        _TagFeel_Enum               = __TagFeel_Enum;
+        _TagType_Enum               = __TagType_Enum;
 
 
 
 
 
-    /*This is a function to add or remove this tag object from its main array.
+        /*Create this tag agnostic object.*/
+        _TagAgnostic_Object = new TagAgnostic_Object();
+
+
+
+
+
+        /*Assign parameters to agnostic object*/
+        _TagAgnostic_Object.general_Bool            = _general_Bool;
+        _TagAgnostic_Object._nameOriginal_String    = _nameOriginal_String;
+
+
+
+
+
+        /*Adjust this tag object name.*/
+        _TagAgnostic_Object.name_String =
+            _TagAgnostic_Object._nameOriginal_String + "_" + Std.string(_TagType_Enum);
+
+
+
+
+        /*Add this object into main array.*/
+        AddOrRemoveThisFromMain_Tag_Object(true);
+
+    }
+    /*==================================================*/
+
+
+
+
+
+    /*==================================================
+    This is a function to add or remove this tag object from its main array.
     There are two main arrays for tag in this program.
     One array is for general tag, an array that is filled with tag with common words.
     One other array is for normal tag, an array that is filled with specific tag of exhibitions.*/
-    private function AddOrRemoveThisFromMain_Tag_Object(_add_Bool:Bool, _general_Bool:Bool):Tag_Object{
+    private function AddOrRemoveThisFromMain_Tag_Object(_add_Bool:Bool):Tag_Object{
 
         var main_Tag_Object_Array:Array<Tag_Object> = null;
-        if(_general_Bool == true){
-
-            main_Tag_Object_Array = _CollectionGlobal_Object.general_Tag_Object_Array;
-
-        }
-        else if(_general_Bool == false){
-
-            main_Tag_Object_Array = _CollectionGlobal_Object._Tag_Object_Array;
-
-        }
+        if(_TagAgnostic_Object.general_Bool == true)
+                { main_Tag_Object_Array = _CollectionGlobal_Object.general_Tag_Object_Array; }
+        else if(_TagAgnostic_Object.general_Bool == false)
+                { main_Tag_Object_Array = _CollectionGlobal_Object._Tag_Object_Array; }
 
 
 
@@ -63,18 +103,12 @@ class Tag_Object{
 
         if(_add_Bool == true){
 
-            if(main_MuseumAndVisitor_Object_Array.indefOf(this) == -1){
-
-                main_Tag_Object_Array.push(this);
-
-            }
+            if(main_MuseumAndVisitor_Object_Array.indefOf(this) == -1)
+                { main_Tag_Object_Array.push(this); }
 
         }
-        else if(_add_Bool == false){
-
-            main_Tag_Object_Array.remove(this);
-
-        }
+        else if(_add_Bool == false)
+                { main_Tag_Object_Array.remove(this); }
 
 
 
@@ -83,11 +117,109 @@ class Tag_Object{
         return this;
 
     }
+    /*==================================================*/
 
 
 
 
 
+    /*==================================================
+    At this moment this GenerateCompanyWord_Void() function will be executed in the main class.
+    PENDING: Make this function directly executed when a tag object is made.*/
+    private function GenerateCompanyWord_Void():Void{
+
+        #if (cpp || neko || php)
+
+            if(
+                _TagType_Enum != ADJ                                        &&
+                _TagType_Enum != ADV                                        &&
+                _TagType_Enum != NOUN_ALIVE_CONCRETE                        &&
+                _TagType_Enum != NOUN_INANIMATE_PLACE_CONCRETE_NO_OWNER
+            ){
+
+                /*PENDING: Add operating system checking, because the command for Linux, Mac, and Windows are each different.*/
+
+
+
+
+
+                var part1_String:String     = "python3 WordnikGetPhrase.py '";              /*The main Python3 command.*/
+                var part2_String:String     = _TagAgnostic_Object.nameOriginal_String;      /*The argument for running thr Python script.*/
+                var part3_String:String     = "' > Phrase.txt";                             /*This terminal command is for generating .txt file that later will be processed by this program.*/
+                var all_String  :String     = part1_String + part2_String + part3_String;   /*All fully funtional terminal command.*/
+                /*Execute the terminal command.*/
+                Sys.command(all_String);
+
+
+
+
+
+                /*Read the .txt file.*/
+                var _FileInput:FileInput = sys.io.File.read("./Phrase.txt", false);
+                /*In this while loop below I am trying to read the line one by one.
+                So each tag will generate like ten company words.
+                Company words are the best word to accompany the tag.
+                The .txt file generated before will have like ten lines.
+                Which each lines is a company word that appears either in the end or in the beginning
+                    of the tag word.*/
+                try{
+
+                    /*Keep reading line until there is no line to read.
+                    If this while loop is returning an error than the loop is ended automatically.*/
+                    while(true){
+
+                        var received_String         :String             = _FileInput.readLine();
+                        var received_String_Array   :Array<String>      = received_String.split("_");
+                        var _CompanyWord_Enum       :CompanyWord_Enum   = Type.createEnum(CompanyWord_Enum, received_String_Array[0]);
+                        var companyWord_String      :String             = received_String_Array[1];
+
+
+
+
+
+                        var _CompanyWord_Struct:CompanyWord_Struct = {
+
+                            _CompanyWord_Enum       :_CompanyWord_Enum,
+                            _TagFeel_Enum           :_TagFeel_Enum,
+                            companyWord_String      :companyWord_String,
+                            nameOriginal_String     :_TagAgnostic_Object.nameOriginal_String
+
+                        };
+
+
+
+
+
+                        _CompanyWord_Struct_Array.push(_CompanyWord_Struct);
+
+                    }
+
+                }
+
+
+
+
+
+                catch(_exception:EoF){}
+
+
+
+
+
+                _FileInput.close();
+
+            }
+
+        #end
+
+    }
+    /*==================================================*/
+
+
+
+
+
+    /*==================================================*/
     private function set__Feel_Enum(__Feel_Enum:TagFeel_Enum):TagFeel_Enum{
 
         _TagFeelEnum = __Feel_Enum;
@@ -95,11 +227,13 @@ class Tag_Object{
         return _TagFeelEnum;
 
     }
-    
+    /*==================================================*/
 
 
 
 
+
+    /*==================================================*/
     private function set__Type_Enum(__Type_Enum):Type_Enum{
 
         _Tag_Enum = __Tag_Enum;
@@ -107,6 +241,7 @@ class Tag_Object{
         return _Type_Enum;
 
     }
+    /*==================================================*/
 
 
 
