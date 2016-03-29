1b4f60ba6f85c3aa2ed55f38cadf747dad26ce09
notalentgeek <mikael.pratama@yahoo.com>
2016-03-29 02:42:38 +0200

    Finished all in the popu remove visitor. Next thing is to create update function for museum add object (this will be a long journey).

--- Source/Core/UI/Popup/VisitorRemoveUIPopup_Object.hx
+++ Source/Core/UI/Popup/VisitorRemoveUIPopup_Object.hx
@@ -43,6 +43,15 @@ class VisitorRemoveUIPopup_Object extends UIPopup_Object{
 
         super.Assign_Void();
 
+
+
+
+        visitor_ListSelector = _Popup.content.findChild(
+            "VisitorRemoveUIPopup_Object_visitor_ListSelector",
+            ListSelector,
+            true
+        );
+
     }
     /*==================================================*/
 
@@ -80,6 +89,34 @@ class VisitorRemoveUIPopup_Object extends UIPopup_Object{
 
         super.Init_Void();
 
+
+
+
+
+        /*Fill all object in the popup object.
+        And assign the default value.*/
+        var loopCounter1_Int:Int = 0;
+        while(loopCounter1_Int < _Global_Object._Visitor_Object_Array.length){
+
+            visitor_ListSelector
+                .dataSource
+                .createFromString(
+                    _Global_Object
+                        ._Visitor_Object_Array[loopCounter1_Int]
+                        ._MuseumAndVisitorAgnostic_Object
+                        .name_String
+                );
+
+
+
+
+
+            loopCounter1_Int ++;
+
+        }
+        visitor_ListSelector.method             = "default";
+        visitor_ListSelector.selectedIndex      = -1;
+
     }
     /*==================================================*/
 
@@ -92,86 +129,8 @@ class VisitorRemoveUIPopup_Object extends UIPopup_Object{
 
         super.Update_Void();
 
-    }
-    /*==================================================*/
 
 
-
-
-
-    /*==================================================*/
-    public function new(__Global_Object:Global_Object, __Root:Root):Void{
-
-        /*Assign the parameters.*/
-        _Global_Object = __Global_Object;
-
-
-
-
-
-        /*Button object click function.*/
-        _Button = __Root.findChild("visitorRemove_Button", Button, true);
-        _Button.onClick = function(__Event){
-
-            /*Button control.*/
-            var buttonControl_Int:Int = 0;
-            buttonControl_Int |= PopupButton.OK;
-            buttonControl_Int |= PopupButton.CANCEL;
-
-
-
-
-
-            /*Popup control.
-            If button clicked this popup appears.*/
-            var _IDisplayObject:IDisplayObject = Toolkit.processXmlResource("layout/VisitorRemoveUIPopup.xml");
-            _Popup = PopupManager.instance.showCustom(_IDisplayObject, "Remove Visitor", buttonControl_Int, function(_button){
-
-                if(_button == PopupButton.OK){
-
-                    /*What happened if you click okay button in the popup.
-                    PENDING: Make this a control function.*/
-                    selected_Visitor_Object = StaticFunction_Collection.Find_Visitor_Object(_Global_Object, selectedVisitorName_String);
-                    selected_Visitor_Object.exhibitionCurrent_Museum_Object = _Global_Object.archiveExhibition_Museum_Object;
-
-                }
-
-            });
-            
-
-
-
-
-            /*Find all object in the popup object.*/
-            visitor_ListSelector = _Popup.content.findChild("VisitorRemoveUIPopup_VisitorListSelector", ListSelector, true);
-
-
-
-
-
-            /*Fill all object in the popup object.
-            And assign the default value.*/
-            var loopCounter1Int:Int = 0;
-            while(loopCounter1Int < _Global_Object.GetVisitorObjectArray().length){
-
-                visitor_ListSelector.dataSource.createFromString(_Global_Object.GetVisitorObjectArray()[loopCounter1Int].GetNameString());
-                loopCounter1Int ++;
-
-            }
-            visitor_ListSelector.method = "default";
-            visitor_ListSelector.selectedIndex = -1;
-
-        }
-
-    }
-    /*==================================================*/
-
-
-
-
-
-    /*==================================================*/
-    public function UpdateVoid():Void{
 
         /*Make sure that the popup and the list visitor object is exist.*/
         if(_Popup != null && visitor_ListSelector != null)
